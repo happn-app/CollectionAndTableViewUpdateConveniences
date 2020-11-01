1b857d8fb5e260f504f89c02c635ba7ad2f60f50
Frizlab <fload@me.com>
2020-11-01 10:14:13 +0100

    Remove Xcode 12 warning

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ import PackageDescription
 
 let package = Package(
 	name: "CollectionAndTableViewUpdateConveniences",
-	platforms: [.iOS(.v8) /* Technically iOS 3! */],
 	products: [
 		.library(name: "CollectionAndTableViewUpdateConveniences", targets: ["CollectionAndTableViewUpdateConveniences"]),
 	],
